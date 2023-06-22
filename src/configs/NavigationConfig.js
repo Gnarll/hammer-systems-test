cbf31049b91533cd9d2cd13843d27409b4be5cd6
Gnarll <vadimfedorovich22@mail.ru>
2023-06-22 23:43:17 +0300

    add clinets bar

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,28 +1,48 @@
-import { DashboardOutlined } from '@ant-design/icons';
-import { APP_PREFIX_PATH } from 'configs/AppConfig'
+import { DashboardOutlined, UserOutlined } from "@ant-design/icons";
+import { APP_PREFIX_PATH } from "configs/AppConfig";
 
+const dashBoardNavTree = [
+  {
+    key: "dashboards",
+    path: `${APP_PREFIX_PATH}/dashboards`,
+    title: "sidenav.dashboard",
+    icon: DashboardOutlined,
+    breadcrumb: false,
+    isGroupTitle: true,
+    submenu: [
+      {
+        key: "dashboards-default",
+        path: `${APP_PREFIX_PATH}/dashboards/default`,
+        title: "sidenav.dashboard.default",
+        icon: DashboardOutlined,
+        breadcrumb: false,
+        submenu: [],
+      },
+    ],
+  },
+];
 
-const dashBoardNavTree = [{
-  key: 'dashboards',
-  path: `${APP_PREFIX_PATH}/dashboards`,
-  title: 'sidenav.dashboard',
-  icon: DashboardOutlined,
-  breadcrumb: false,
-  isGroupTitle: true,
-  submenu: [
-    {
-      key: 'dashboards-default',
-      path: `${APP_PREFIX_PATH}/dashboards/default`,
-      title: 'sidenav.dashboard.default',
-      icon: DashboardOutlined,
-      breadcrumb: false,
-      submenu: []
-    }
-  ]
-}]
+const pagesNavTree = [
+  {
+    key: "pages",
+    path: `${APP_PREFIX_PATH}/pages`,
+    title: "sidenav.pages",
+    icon: UserOutlined,
+    breadcrumb: false,
+    isGroupTitle: true,
+    submenu: [
+      {
+        key: "pages-clients",
+        path: `${APP_PREFIX_PATH}/pages/clients`,
+        title: "Clients",
+        icon: UserOutlined,
+        breadcrumb: false,
+        submenu: [],
+      },
+    ],
+  },
+];
 
-const navigationConfig = [
-  ...dashBoardNavTree
-]
+const navigationConfig = [...dashBoardNavTree, ...pagesNavTree];
 
 export default navigationConfig;
